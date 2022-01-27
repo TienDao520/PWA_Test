decd53778a74dfedf449a6fcb3259b5416b189c0
Tien Dao <tiendao5240@gmail.com>
2022-01-27 13:56:24 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,52 @@
+const elements = {
+  h1: document.querySelector("h1"),
+  titleInput: document.querySelector("#title"),
+  textInput: document.querySelector("#text"),
+  urlInput: document.querySelector("#url"),
+  fileInput: document.querySelector("#files"),
+  shareBtn: document.querySelector("#share"),
+};
+
+if (window.location.href.includes("article")) {
+  const parseURL = new URL(window.location);
+  console.log("parseURL: " + parseURL);
+  const protocolURL = parseURL.searchParams.get("id");
+  console.log("protocolURL: " + protocolURL);
+  const articleId = protocolURL.replace("web+pwa://", "").replace("/", "");
+  console.log("articleId: " + articleId);
+  document.querySelector("h1").innerHTML = `Article Id: ${articleId}`;
+  //https://dev.localhost/article/?id=web+pwa://a323123/
+}
+
+const onShare = async () => {
+  const title = elements.titleInput.value;
+  const text = elements.textInput.value;
+  const url = elements.urlInput.value;
+
+  try {
+    if (navigator.share) {
+      await navigator.share({
+        title,
+        text,
+        url,
+        //it is the same with key:value
+        //title:title, text:text, url:url
+      });
+      console.log("Opened share sheet successfully", { title, text, url });
+    }
+  } catch (err) {
+    console.error(`Could not open share dialog`, err);
+  }
+};
+
+const onShareFile = async () => {
+  const title = elements.titleInput.value;
+  const text = elements.textInput.value;
+  const url = elements.urlInput.value;
+  try {
+    if (navigator.canShare && navigator.canShare({ files: [] })) {
+    }
+  } catch (err) {}
+};
+
+elements.shareBtn.addEventListener("click", onShare);
